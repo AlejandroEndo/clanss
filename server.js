a7ac086f3695740a0972ade8ec7f7d6533477575
AlejandroEndo <alejoendo@hotmail.com>
2017-05-08 19:21:29 -0500

    Ahora deberia funcionar...

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
 /**
  * Created by Endo on 8/05/2017.
  */
+
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+app.listen(port, function() {
+    console.log('app running');
+});
